08b174a233b480f4fc64c2018589e45b66b2df26
1F10240084 <s1F102400842@iniad.org>
2025-07-03 13:40:35 +0900

    修正

--- todo/tests.py
+++ todo/tests.py
@@ -52,7 +52,7 @@ class TaskModelTestCase(TestCase):
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
-        response = client()
+        response = client.get('/')
         
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
@@ -68,7 +68,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context['tasks' ]), 1)
 
     def test_index_get_order_post(self):
-        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
         task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
@@ -76,12 +76,12 @@ class TodoViewTestCase(TestCase):
         response = client.get('/?order=post')
 
         self.assertEqual(response.status_code, 200) 
-        self.assertEqual(response.templates[0].name,'todo/index.htmI')
+        self.assertEqual(response.templates[0].name,'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context ['tasks'][1], task1)
 
     def test_index_get_order_due(self):
-        taskl = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
         task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1))) 
         task2.save()
@@ -89,7 +89,7 @@ class TodoViewTestCase(TestCase):
         response = client.get ('/?order=due')
 
         self.assertEqual(response.status_code, 200) 
-        self.assertEqual(response.templates[0].name, 'todo/index.htm]')
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
 
